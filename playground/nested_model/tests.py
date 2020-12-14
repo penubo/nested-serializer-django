2b9fa7d837503a6a850bdb559e99fd2bb29d1ad6
yongjoon <yongjoon@bearrobotics.ai>
2020-12-14 18:18:57 +0900

    made nested_model app

---
+++ playground/nested_model/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+class ObjectModelTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_testworks(self):
+
+        self.assertTrue(True)
